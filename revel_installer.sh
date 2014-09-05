ddf0eec01f1e7a4323afc705febc511fcdb3d1be
qmaxquique <qmax.mail@gmail.com>
2014-09-04 23:41:20 -0300

    code reorganization

--- revel_installer.sh
+++ revel_installer.sh
@@ -2,20 +2,37 @@
 # Script to deploy Revel Framework with running examples app at Terminal.com
 # https://github.com/revel/revel 
 
-INSTALL_PATH="/var/www"
+# Basic Functions
+install(){
+	# Includes
+	wget https://raw.githubusercontent.com/qmaxquique/terminal.com/master/terlib.sh
+	source terlib.sh || (echo "cannot get the includes"; exit -1)
 
-# Includes
-wget https://raw.githubusercontent.com/qmaxquique/terminal.com/master/terlib.sh
-source terlib.sh || (echo "cannot get the includes"; exit -1)
+	# Basics
+	pkg_update
+	system_cleanup
+	basics_install
 
-# Basics
-pkg_update
-system_cleanup
-basics_install
+	# Install dependences 
+	golang_install
+	gvm install go1.2
 
-# 1 Install dependences 
-golang_install
-gvm install go1.2
+	# Install Revel
+	source ~/.profile
+	go get github.com/revel/cmd/revel && echo "Revel was succesfully installed"
+	}
 
-# Install Revel
-go get github.com/revel/cmd/revel
+show_sample(){
+	nohup revel run github.com/revel/revel/samples/booking &
+	export PATH=$PATH:/srv/cloudlabs/scripts
+	browse.sh http://localhost:9000
+}
+
+#Main
+if [[ -z $1 ]]; then
+	install && show_samples
+elif [[ $1 == "show" ]]; then 
+	show_samples
+else
+	echo "unknown parameter specified"
+fi
