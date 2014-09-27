8af90bb7e65eb5a588837b64f4b72476abfa0051
qmaxquique <qmax.mail@gmail.com>
2014-09-26 22:02:58 -0300

    prestashop initial commit

---
+++ prestashop_installer.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Script to deploy Wallabag at Terminal.com
+
+INSTALL_PATH="/var/www"
+
+# Includes
+wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
+source terlib.sh || (echo "cannot get the includes"; exit -1)
+
+install(){
+	# Basics
+	pkg_update
+	system_cleanup
+	basics_install
+
+	# Procedure: 
+	php5_install
+	mysql_install
+	mysql_config prestashop prestashop terminal
+	cd $INSTALL_PATH
+	wget -q https://raw.githubusercontent.com/terminalcloud/apps/master/others/prestashop_1.6.0.9.zip
+	unzip prestashop_1.6.0.9.zip
+	rm prestashop_1.6.0.9.zip
+	chown -R www-data:www-data prestashop
+	apache_install
+	apache_default_vhost prestashop.conf $INSTALL_PATH/prestashop
+	sed -i 's/upload_max_filesize\ \=\ 2M/upload_max_filesize\ \=\ 20M/g' /etc/php5/apache2/php.ini
+	sed -i 's/post_max_size\ \=\ 8M/post_max_size\ \=\ 24M/g' /etc/php5/apache2/php.ini
+	service apache2 restart
+}
+
+show(){
+	# Get the startup script
+	wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/prestashop_hooks.sh
+	mkdir -p /CL/hooks/
+	mv prestashop_hooks.sh /CL/hooks/startup.sh
+	# Execute startup script by first to get the common files
+	chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
+}
+
+if [[ -z $1 ]]; then
+	install && show
+elif [[ $1 == "show" ]]; then 
+	show
+else
+	echo "unknown parameter specified"
+fi
