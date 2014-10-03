fbebbf5b1ee50b549be36c84497ef722d20ab84f
qmaxquique <qmax.mail@gmail.com>
2014-10-03 18:12:45 -0300

    bpf initial commit

---
+++ bpf_installer.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Script to deploy Bottle Python Framework at Terminal.com
+
+INSTALL_PATH="/var/www/html"
+
+# Includes
+wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
+source terlib.sh || (echo "cannot get the includes"; exit -1)
+
+
+install(){
+	# Basics
+	pkg_update
+	system_cleanup
+	basics_install
+
+	# Procedure: 
+	mysql_install
+	python_install
+	mysql_setup ushahidi ushahidi terminal
+	apt-get -y install libapache2-mod-wsgi libmysqlclient-dev python-dev
+ || yum -y install httpd-mod-wsgi libmysqlclient-dev python-dev
+	apache_install
+	# Vhost config
+	[[ -f /etc/debian_version ]] && vpath="/etc/apache2/sites-available/" || vpath="/etc/httpd/config.d/"
+	cat > $vpath/todo.conf << EOF
+<VirtualHost *:80>
+        WSGIScriptAlias / /var/www/html/todo.py
+</VirtualHost>
+EOF
+	if [[ -f /etc/debian_version ]]; then
+		[[ -f /etc/apache2/sites-enabled/000-default.conf ]] && rm /etc/apache2/sites-enabled/000-default.conf
+		ln -s /etc/apache2/sites-available/todo.conf /etc/apache2/sites-enabled/todo.conf 
+		service apache2 restart 
+	else
+		[[ -f /etc/httpd/conf.d/000-default.conf ]] && rm /etc/httpd/conf.d/000-default.conf
+		service httpd restart
+	fi
+
+	# Apache conf
+	[[ -f /etc/debian_version ]] && echo "WSGIPythonPath /var/www/html/" >> /etc/apache2/apache.conf || echo "WSGIPythonPath /var/www/html/" >> /etc/httpd/httpd.conf
+	mysql -uroot -proot -e"CREATE TABLE todo (id INTEGER PRIMARY KEY AUTO_INCREMENT, task char(100) NOT NULL, status bool NOT NULL);"
+	cd $INSTALL_PATH
+	pip install bottle
+	pip install SQLAlchemy
+	pip install mysql-python
+	git clone https://github.com/mcapielo/Todo-List-Bottle-SQLAlchemy-Bootstrap.git .
+	chown -R www-data:www-data $INSTALL_PATH/
+}
+
+
+show(){
+	# Get the startup script
+	wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/bpf_hooks.sh
+	mkdir -p /CL/hooks/
+	mv ushahidi_hooks.sh /CL/hooks/startup.sh
+	# Execute startup script by first to get the common files
+	chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
+}
+
+if [[ -z $1 ]]; then
+	install && show
+elif [[ $1 == "show" ]]; then 
+	show
+else
+	echo "unknown parameter specified"
+fi
---
+++ docs/bpf.md
@@ -0,0 +1,43 @@
+# **BottlePy** Terminal.com Snapshot
+*Python Web Framework*
+
+---
+
+## About BottlePy
+Bottle is a fast, simple and lightweight WSGI micro web-framework for [Python](http://python.org/). It is distributed as a single file module and has no dependencies other than the [Python Standard Library](http://docs.python.org/library/).
+
+### Features
+- **Routing** - Requests to function-call mapping with support for clean and dynamic URLs.
+
+- **Templates:** Fast and pythonic [built-in template engine](http://bottlepy.org/docs/dev/tutorial.html#tutorial-templates) and support for [mako](http://www.makotemplates.org/), [jinja2](http://jinja.pocoo.org/) and [cheetah](http://www.cheetahtemplate.org/) templates
+
+- **Utilities:** Convenient access to form data, file uploads, cookies, headers and other HTTP-related metadata.
+
+- **Server:** Built-in HTTP development server and support for [paste](http://pythonpaste.org/), [fapws3](https://github.com/william-os4y/fapws3), [bjoern](https://github.com/jonashaag/bjoern), [gae](https://developers.google.com/appengine/), [cherrypy](http://www.cherrypy.org/) or any other [WSGI](http://www.wsgi.org/) capable HTTP server.
+
+
+---
+
+## Usage
+Spin up your container based on this snapshot. You will see on screen the file browser and editor ready to work over the working example.
+
+If you want, you can start a new project by moving or deleting the contents of /var/www/html and then installing the framework again executing `pip install bottle`
+
+
+## Documentation
+- [BottlePy Main Website](http://bottlepy.org/)
+- [Mailing list](bottlepy@googlegroups.com)
+- [A simple todo-app tutorial](http://bottlepy.org/docs/dev/tutorial_app.html)
+- [Docs and Tutorials](http://bottlepy.org/docs/dev/tutorial.html)
+
+---
+
+### Additional Information
+#### Ushahidi Terminal.com container automatic installation:
+You can replicate this container from a [base snapshot](https://www.terminal.com/tiny/FzpHiTXG1K) by executing:
+`wget https://raw.githubusercontent.com/terminalcloud/apps/master/bfp_installer.sh && bash bfp_installer.sh`
+
+
+---
+
+#### Thanks for using (and coding) BottlePy at Terminal.com!
