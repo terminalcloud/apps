17b7b8090d3badbdd2a17fe767ddd8d1dc3e89a9
qmaxquique <qmax.mail@gmail.com>
2014-10-03 15:40:01 -0300

    minor changes

--- ushahidi_installer.sh
+++ ushahidi_installer.sh
@@ -16,7 +16,7 @@ install(){
 	# Procedure: 
 	php5_install
 	mysql_install
-	mysql_setup ushahidi terminal
+	mysql_setup ushahidi ushahidi terminal
 	apt-get -y install php5-imap rails
 	cd $INSTALL_PATH
 	git clone https://github.com/ushahidi/platform.git ushahidi
@@ -25,9 +25,13 @@ install(){
 	apache_default_vhost ushahidi.conf $INSTALL_PATH/ushahidi
 	chown -R www-data:www-data $INSTALL_PATH/ushahidi
 	curl http://npmjs.org/install.sh | sh
-	npm install bundle
-	cd ushahidi
+	cd $INSTALL_PATH 
+	npm install -g bundle
+	bower install --allow-root
 	bin/update
+	cd ..
+	cp $INSTALL_PATH/ushahidi/httpdocs/template.htaccess $INSTALL_PATH/ushahidi/httpdocs/.htaccess
+	#application/config/database.php
 	sed -i 's/upload_max_filesize\ \=\ 2M/upload_max_filesize\ \=\ 20M/g' /etc/php5/apache2/php.ini
 	sed -i 's/post_max_size\ \=\ 8M/post_max_size\ \=\ 24M/g' /etc/php5/apache2/php.ini
 	service apache2 restart
@@ -37,7 +41,7 @@ show(){
 	# Get the startup script
 	wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/ushahidi_hooks.sh
 	mkdir -p /CL/hooks/
-	mv prestashop_hooks.sh /CL/hooks/startup.sh
+	mv ushahidi_hooks.sh /CL/hooks/startup.sh
 	# Execute startup script by first to get the common files
 	chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
 }
