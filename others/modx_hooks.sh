1dec4610322ec1b8c67d32f657a8d048c8031e10
qmaxquique <qmax.mail@gmail.com>
2014-10-08 12:20:10 -0300

    minor changes

--- others/modx_hooks.sh
+++ others/modx_hooks.sh
@@ -15,7 +15,7 @@ cat > /root/info.html << EOF
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="termlib.css" />
-<p id="exlink"><a id="exlink" target="_blank" href="http://$(hostname)-80.terminal.com"><b>Check your installation here!</b></a></p>
+<p id="exlink"><a id="exlink" target="_blank" href="http://$(hostname)-80.terminal.com/manager"><b>Check your installation here!</b></a></p>
 </head>
 <body>
 EOF
