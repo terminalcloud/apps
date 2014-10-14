a4096a82b5bb8137fc2912d5a2d938a8b8554ed1
qmaxquique <qmax.mail@gmail.com>
2014-10-14 12:36:29 -0300

    initial commit

---
+++ mahara_installer.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Script to deploy Mahara at Terminal.com
+
+INSTALL_PATH="/var/www"
+
+# Includes
+wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
+source terlib.sh || (echo "cannot get the includes"; exit -1)
+
+install(){
+	# Basics
+	pkg_update
+	system_cleanup
+	basics_install
+
+	# Procedure: 
+	php5_install
+	mysql_install
+	mysql_setup mahara mahara terminal
+	cd $INSTALL_PATH
+	wget https://launchpad.net/mahara/1.9/1.9.2/+download/mahara-1.9.2.zip
+	unzip mahara-1.9.2.zip && rm mahara-1.9.2.zip
+	mv mahara-1.9.2 mahara
+	chown -R www-data:www-data mahara
+	apache_install
+	apache_default_vhost mahara.conf $INSTALL_PATH/mahara
+	sed -i 's/upload_max_filesize\ \=\ 2M/upload_max_filesize\ \=\ 25M/g' /etc/php5/apache2/php.ini
+	sed -i 's/post_max_size\ \=\ 8M/post_max_size\ \=\ 32M/g' /etc/php5/apache2/php.ini
+	service apache2 restart
+}
+
+show(){
+	# Get the startup script
+	wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/mahara_hooks.sh
+	mkdir -p /CL/hooks/
+	mv mahara_hooks.sh /CL/hooks/startup.sh
+	# Execute startup script by first to get the common files
+	chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
+}
+
+if [[ -z $1 ]]; then
+	install && show
+elif [[ $1 == "show" ]]; then 
+	show
+else
+	echo "unknown parameter specified"
+fi
