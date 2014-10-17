3e20e7d4978c9a77a0bbeafde6268ca0a7d5235c
qmaxquique <qmax.mail@gmail.com>
2014-10-17 14:59:10 -0300

    minor changes

--- others/nightmarejs_example.js
+++ others/nightmarejs_example.js
@@ -1,21 +1,23 @@
-// Receive arguments from the console
-var arguments = process.argv.slice(2);
-console.log("Searching: " + arguments)
-
 var Nightmare = require('nightmare');
+var exec = require('child_process').exec;
+ 
+// Receive arguments from the console.
+var arguments = process.argv.slice(2);
+console.log("Searching: " + arguments);
+ 
 new Nightmare()
-	.goto('http://yahoo.com')
-	.type('input[title="Search"]', arguments)
-	.click('.searchsubmit')
-	// Wait until this is done before take the snapshot.
-	.wait(5000)
-	.screenshot('./scr.jpg')
-	.run(function (err, nightmare) {
-		if (err) return console.log(err);
-		var exec = require('child_process').exec;
-		console.log('Displaying results')
-		// Fork a script to show the screenshot on the browser IDE component.
-		exec('/srv/cloudlabs/scripts/display.sh scr.jpg', function callback(error, stdout, stderr){ });
-		console.log('Done!');
-	});
-
+  .goto('http://yahoo.com')
+  .type('input[title="Search"]', arguments)
+  .click('.searchsubmit')
+ 
+  // Wait until the page loads, then take a screenshot.
+  .wait()
+  .screenshot('./scr.jpg')
+  .run(function (err, nightmare) {
+    if (err) return console.log(err);
+    console.log('Displaying results');
+    
+    // Fork the process to show the screenshot in the IDE browser.
+    exec('/srv/cloudlabs/scripts/display.sh scr.jpg');
+    console.log('Done!');
+  });
