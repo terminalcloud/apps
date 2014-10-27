8e7831aeaa1f6d9ddde4b8b050cf13c22e7157d0
qmaxquique <qmax.mail@gmail.com>
2014-10-27 11:59:42 -0300

    initial commit

---
+++ wide_installer.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Script to deploy wide PM at Terminal.com
+
+INSTALL_PATH="/root"
+
+# Includes
+wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
+source terlib.sh || (echo "cannot get the includes"; exit -1)
+
+install(){
+	# Basics
+	pkg_update
+	system_cleanup
+	basics_install
+
+	# Procedure: 
+	golang_install
+	git clone https://github.com/b3log/wide.git
+	cd wide
+	go get -u
+	go get -u github.com/88250/ide_stub
+	go get -u github.com/nsf/gocode
+	go build
+	#./wide
+}
+
+show(){
+	# Get the startup script
+	wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/wide_hooks.sh
+	mkdir -p /CL/hooks/
+	mv wide_hooks.sh /CL/hooks/startup.sh
+	# Execute startup script by first to get the common files
+	chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
+}
+
+if [[ -z $1 ]]; then
+	install && show
+elif [[ $1 == "show" ]]; then 
+	show
+else
+	echo "unknown parameter specified"
+fi
