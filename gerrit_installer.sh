503c5c2402167c6d90c923436c669a5cee48ef25
qmaxquique <qmax.mail@gmail.com>
2014-10-29 13:52:48 -0300

    minor changes

--- gerrit_installer.sh
+++ gerrit_installer.sh
@@ -21,9 +21,9 @@ install(){
 
 show(){
 	# Get the startup script
-	wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/gerit_hooks.sh
+	wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/gerrit_hooks.sh
 	mkdir -p /CL/hooks/
-	mv gerit_hooks.sh /CL/hooks/startup.sh
+	mv gerrit_hooks.sh /CL/hooks/startup.sh
 	# Execute startup script by first to get the common files
 	chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
 }
