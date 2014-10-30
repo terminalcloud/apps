9a796c8b4a3499e1878f9c75aad786f37bdbceb0
qmaxquique <qmax.mail@gmail.com>
2014-10-29 23:28:28 -0300

    initial commit

---
+++ docs/imbers.md
@@ -0,0 +1,50 @@
+# **Imbers** Terminal.com Snapshot
+*Imbers, the easiest way to meet exciting new people.*
+
+---
+
+## About Imbers
+**Imbers** is an open source project that was made during the [ProductHunt Hackathon](http://www.producthunt.com/) 2014.
+With this application you can meet someone new every day!
+
+## About this Imbers Snapshot
+This snapshot was made from the Imber's git repositories as an option to the default docker installation.
+
+
+---
+
+## Usage
+
+Just spin-up your container based on this snapshot and click over "Check your installation here".
+
+
+### Imbers location 
+
+- Backend: `/root/imbers-backend`
+- Frontend: `/root/imbers-frontend`
+
+### Start commands for Imbers
+`cd /root/imbers-backend && foreman start`
+`cd /root/imbers-frontend && npm start`
+
+---
+
+![1](http://www.imbers.com/img/header_screen.png)  
+
+---
+
+## Documentation
+- [Imbers Main Website](http://www.imbers.com/)
+- [Github Repos](https://github.com/52unicorns/)
+
+---
+
+### Additional Information
+
+#### Imbers Terminal.com container automatic installation:
+You can replicate this container from a [base snapshot](https://www.terminal.com/tiny/FzpHiTXG1K) by executing:
+`wget https://raw.githubusercontent.com/terminalcloud/apps/master/Imbers_installer.sh && bash Imbers_installer.sh`
+
+---
+
+#### Thanks for using try Imbers at Terminal.com!
--- imbers_installer.sh
+++ imbers_installer.sh
@@ -13,20 +13,64 @@ install(){
 	system_cleanup
 	basics_install
 
-	# Procedure:
+	# Backend:
+	cd $INSTALL_PATH
+	gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 	curl -L get.rvm.io | bash -s stable # Requires Basics
 	source /usr/local/rvm/scripts/rvm
 	echo "source /usr/local/rvm/scripts/rvm" >> .bashrc
 	rvm install 2.1.1
-	rvm use current 2.1.1
+	rvm use 2.1.1
 	rvm rubygems current
 	apt -y install postgresql redis-server
 	wget -O - http://debian.neo4j.org/neotechnology.gpg.key | apt-key add -
 	echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
 	apt-get update
-	apt-get install neo4j
+	apt-get -y install neo4j
 	git clone https://github.com/52unicorns/imbers-backend.git
+	cd imbers-backend
+	cp .env.sample .env
+	echo "Now go and edit the $INSTALL_PATH/imbers-backend/.env file in other terminal and back here when it's done"
+	read
+	apt-get -y install build-essential libpq-dev postgresql-contrib-9.3
+	gem install bundler
+	gem install rake
+	gem install i18n
+	bundle install
+	# nohup foreman start &
+
+	#Frontend
+	cd $INSTALL_PATH
+	git clone https://github.com/52unicorns/imbers-www.git
+	apt-get -y install nodejs
+	cp .env.sample .env
+	echo "Now go and edit the $INSTALL_PATH/imbers-www/.env file in other terminal and back here when it's done"
+	read
+	npm install
+	#npm start
+
+
 }
+
+#Postgres Fix (If your template DB is not in UTF8 )
+#postgres=# update pg_database set datallowconn = TRUE where datname = 'template0';
+#UPDATE 1
+#postgres=# \c template0
+#You are now connected to database "template0" as user "postgres".
+#template0=# update pg_database set datistemplate = FALSE where datname = 'template1';
+#UPDATE 1
+#template0=#  drop database template1;
+#DROP DATABASE
+#template0=#  create database template1 with template = template0 encoding = 'UTF8';
+#CREATE DATABASE
+#template0=# update pg_database set datistemplate = TRUE where datname = 'template1';
+#UPDATE 1
+#template0=#  \c template1
+#You are now connected to database "template1" as user "postgres".
+#template1=#  update pg_database set datallowconn = FALSE where datname = 'template0';
+#UPDATE 1
+#template1=# \q
+
 
 
 show(){
@@ -39,7 +83,7 @@ show(){
 }
 
 if [[ -z $1 ]]; then
-	install #&& show
+	install && show
 elif [[ $1 == "show" ]]; then 
 	show
 else
