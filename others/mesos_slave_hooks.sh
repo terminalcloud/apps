82219c266d016efd03be348dd5bdf41313e1dd1a
qmaxquique <qmax.mail@gmail.com>
2014-10-31 16:30:39 -0300

    minor changes

--- others/mesos_slave_hooks.sh
+++ others/mesos_slave_hooks.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:/srv/cloudlabs/scripts
 # Getting the doc and styles
 wget -q -N --timeout=2 https://raw.githubusercontent.com/terminalcloud/apps/master/docs/"$name".md
 wget -q -N --timeout=2 https://raw.githubusercontent.com/terminalcloud/apps/master/docs/termlib.css && mv termlib.css /root/
-wget -q -N --timeout=2 https://raw.githubusercontent.com/terminalcloud/apps/master/others/mesos_slave_cfg.sh && mv mesos_slave_cfg.sh /root/
+wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/mesos_slave_cfg.sh && mv mesos_slave_cfg.sh /root/
 
 # Making the file...
 cat > /root/info.html << EOF
