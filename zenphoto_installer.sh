22b43d100b511954dda5b77dfff3be373e3c21e7
qmaxquique <qmax.mail@gmail.com>
2014-11-05 17:22:17 -0300

    initial commit

---
+++ zenphoto_installer.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Script to deploy ZenPhoto at Terminal.com
+
+INSTALL_PATH="/var/www"
+
+# Includes
+wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
+source terlib.sh || (echo "cannot get the includes"; exit -1)
+
+install(){
+  # Basics
+  pkg_update
+  system_cleanup
+  basics_install
+
+  # Procedure:
+  php5_install
+  mysql_install
+  mysql_setup zenphoto zenphoto terminal
+  apt-get -y install php5-ldap php5-imap
+  php5enmod imap
+  cd $INSTALL_PATH
+  wget https://github.com/zenphoto/zenphoto/archive/zenphoto-1.4.6.zip
+  unzip zenphoto_1801.zip && rm zenphoto_1801.zip
+  mv zenphoto_1801 zenphoto
+  chown -R www-data:www-data zenphoto
+  apache_install
+  apache_default_vhost zenphoto.conf $INSTALL_PATH/zenphoto
+  sed -i 's/upload_max_filesize\ \=\ 2M/upload_max_filesize\ \=\ 25M/g' /etc/php5/apache2/php.ini
+  sed -i 's/post_max_size\ \=\ 8M/post_max_size\ \=\ 32M/g' /etc/php5/apache2/php.ini
+  sed -i 's/memory_limit\ \=\ 128M/memory_limit\ \=\ 256M/g' /etc/php5/apache2/php.ini
+  service apache2 restart
+}
+
+show(){
+  # Get the startup script
+  wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/zenphoto_hooks.sh
+  mkdir -p /CL/hooks/
+  mv zenphoto_hooks.sh /CL/hooks/startup.sh
+  # Execute startup script by first to get the common files
+  chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
+}
+
+if [[ -z $1 ]]; then
+  install && show
+elif [[ $1 == "show" ]]; then
+  show
+else
+  echo "unknown parameter specified"
+fi
