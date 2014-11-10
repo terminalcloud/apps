524ac84f58e2079f73f99bf83848e6148211681e
qmaxquique <qmax.mail@gmail.com>
2014-11-10 18:38:14 -0300

    version update

--- docs/wordpress.md
+++ docs/wordpress.md
@@ -1,24 +1,47 @@
-# Welcome to WordPress 4 Terminal
+# **WordPress** Terminal.com Snapshot
+
+*Full featured Blog Tool, Publishing Platform, and CMS*
+
+---
+
 ## About WordPress
-[WordPress](http://wordpress.org/) is web software you can use to create a beautiful website or blog. We like to say that WordPress is both free and priceless at the same time.
+
+[WordPress](http://wordpress.org/) is web software you can use to create a beautiful website or blog.
+
 The core software is built by hundreds of community volunteers and when you’re ready for more there are [thousands of plugins](http://wordpress.org/plugins/) and [themes](http://wordpress.org/themes/) available to transform your site into almost [anything you can imagine](http://wordpress.org/showcase/). Over 60 million people have chosen WordPress to power the place on the web they call “home” — we’d love you to join the family.
 
-## Usage:
-### Basics
-Just spin up your Terminal based on this [Terminal.com](https://www.terminal.com) snapshot and point your browser to http://yourteminalname-80.terminal.com to start configuring you wordpress instance.
 
-### Learning WordPress
+---
+
+## Usage
+
+Just spin up a new Terminal based on this snapshot. Access the admin section by clicking to "Finish your installation here" and use the db credentials provided (see below).
+
+
+### MySQL DB Credentials
+
+- username: `wordpress`
+- password: `terminal`
+
+---
+
+![1](https://s.w.org/screenshots/3.9/dashboard.png)
+
+---
+
+## Documentation
+
 On this links you will find useful information about WordPress
 - [Official Documentation](http://codex.wordpress.org/Main_Page)
 - [WordPress Lessons](http://codex.wordpress.org/WordPress_Lessons)
 - [Code Reference](http://developer.wordpress.org/reference)
 
-### Additional Information
-#### Wordpress Terminal automatic installation:
-`wget https://raw.githubusercontent.com/terminalcloud/apps/master/wordpress_installer.sh && bash wordpress_installer.sh`
-#### Root MySQL Credentials
-- user: root
-- pass: root
+---
+
+### Snapshot Bootstrap Script
+
+This snapshot was created from a [base snapshot](https://www.terminal.com/tiny/FzpHiTXG1K) by executing:
+`wget https://raw.githubusercontent.com/terminalcloud/apps/master/WordPress_installer.sh && bash WordPress_installer.sh`
 
 ---
 
--- wordpress_installer.sh
+++ wordpress_installer.sh
@@ -1,60 +1,66 @@
 #!/bin/bash
-# Script to deploy Wordpress in a Terminal.com container
+# Script to deploy Wordpress at Terminal.com
+
 INSTALL_PATH="/var/www"
 
+# Includes
+wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
+source terlib.sh || (echo "cannot get the includes"; exit -1)
+
 install(){
-	DB_NAME="wp"
-	DB_USER="wp"
-	DB_PASS="terminal"
-
-	# Includes
-	wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
-	source terlib.sh || (echo "cannot get the includes"; exit -1)
-
 	# Basics
 	pkg_update
 	system_cleanup
 	basics_install
 
-	# Procedure: 
-	# 1 - Get prerrequisites.
-	apache_install
+	# Procedure:
 	php5_install
 	mysql_install
-	mysql_setup $DB_NAME $DB_USER $DB_PASS
-	apache_default_vhost wordpress.conf $INSTALL_PATH/wordpress
-
-	# 2 - Install the product
+	mysql_setup wordpress wordpress terminal
 	cd $INSTALL_PATH
-	wget http://wordpress.org/latest.tar.gz
-	tar xzvf latest.tar.gz
-	rm latest.tar.gz
+  wget http://wordpress.org/latest.tar.gz
+  tar xzvf latest.tar.gz && rm latest.tar.gz
+	chown -R www-data:www-data wordpress
+	apache_install
+	apache_default_vhost wordpress.conf $INSTALL_PATH/wordpress
+	sed -i 's/upload_max_filesize\ \=\ 2M/upload_max_filesize\ \=\ 25M/g' /etc/php5/apache2/php.ini
+	sed -i 's/post_max_size\ \=\ 8M/post_max_size\ \=\ 32M/g' /etc/php5/apache2/php.ini
+	sed -i 's/memory_limit\ \=\ 128M/memory_limit\ \=\ 256M/g' /etc/php5/apache2/php.ini
 
-	# 3 - Configuring...
-	cd $INSTALL_PATH/wordpress
-	cp wp-config-sample.php wp-config.php
-	sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
-	sed -i "s/username_here/$DB_USER/g" wp-config.php
-	sed -i "s/password_here/$DB_PASS/g" wp-config.php
+  # 3 - Configuring...
+  cd $INSTALL_PATH/wordpress
+  cp wp-config-sample.php wp-config.php
+  sed -i "s/database_name_here/wordpress/g" wp-config.php
+  sed -i "s/username_here/wordpress/g" wp-config.php
+  sed -i "s/password_here/terminal/g" wp-config.php
 
-	mkdir $INSTALL_PATH/wordpress/wp-content/uploads
-	chown -R www-data:www-data $INSTALL_PATH/wordpress
-	clear
-	start_hooks_install # This to show the link to the app automatically
-	echo "Wordpress installation finished."	
+  mkdir $INSTALL_PATH/wordpress/wp-content/uploads
+  chown -R www-data:www-data $INSTALL_PATH/wordpress
+
+	service apache2 restart
+
+  # 4 - Install wp-cli
+  cd /root
+  wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+  chmod +x wp-cli.phar
+  mv wp-cli.phar /usr/local/bin/wp
+  wget https://github.com/wp-cli/wp-cli/raw/master/utils/wp-completion.bash
+  mv wp-completion.bash .wp-completion.bash
+  echo "source .wp-completion.bash" >> .bashrc
 }
 
 show(){
-	wget -q https://raw.githubusercontent.com/terminalcloud/apps/master/docs/wordpress.md
-	export PATH=$PATH:/srv/cloudlabs/scripts
-	edit.sh wordpress.md
-	cd.sh /var/www/wordpress/ ## Show the Wordpress directory
-	/CL/hooks/startup.sh
+	# Get the startup script
+	wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/wordpress_hooks.sh
+	mkdir -p /CL/hooks/
+	mv wordpress_hooks.sh /CL/hooks/startup.sh
+	# Execute startup script by first to get the common files
+	chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
 }
 
 if [[ -z $1 ]]; then
 	install && show
-elif [[ $1 == "show" ]]; then 
+elif [[ $1 == "show" ]]; then
 	show
 else
 	echo "unknown parameter specified"
