6be8fe55977151d3d289551f737cb4ce7d867a78
qmaxquique <qmax.mail@gmail.com>
2014-11-11 19:28:47 -0300

    initial commit

---
+++ docs/subrion.md
@@ -0,0 +1,48 @@
+# **Subrion** Terminal.com Snapshot
+
+*MINI_DESC*
+
+---
+
+## About Subrion
+
+Subrion is
+
+---
+
+## Main Features
+
+- FEATURES
+
+---
+
+## Usage
+
+Just spin up a new Terminal based on this snapshot. Access the admin section by clicking to "Subrion Admin login" and login with the credentials provided (see below).
+
+### Credentials
+
+- username: `admin`
+- password: `t3rminal`
+
+---
+
+![1](IMAGE_URL)
+
+---
+
+## Documentation
+
+- [Subrion Website]()
+- [Documentation]()
+
+---
+
+### Snapshot Bootstrap Script
+
+This snapshot was created from a [base snapshot](https://www.terminal.com/tiny/FzpHiTXG1K) by executing:
+`wget https://raw.githubusercontent.com/terminalcloud/apps/master/subrion_installer.sh && bash subrion_installer.sh`
+
+---
+
+#### Thanks for using Subrion at Terminal.com!
---
+++ subrion_installer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Script to deploy Subrion at Terminal.com
+
+INSTALL_PATH="/var/www"
+
+# Includes
+wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
+source terlib.sh || (echo "cannot get the includes"; exit -1)
+
+install(){
+  # Basics
+  pkg_update
+  system_cleanup
+  basics_install
+
+  # Procedure:
+  php5_install
+  mysql_install
+  mysql_setup subrion subrion terminal
+  mkdir -p $INSTALL_PATH/subrion
+  cd $INSTALL_PATH
+  wget -O subrion.zip http://tools.subrion.org/get/latest.zip
+  unzip subrion.zip && rm subrion.zip
+  chown -R www-data:www-data subrion
+  apache_install
+  apache_default_vhost subrion.conf $INSTALL_PATH/subrion
+  echo "date.timezone = America/Los_Angeles" >> /etc/php5/apache2/php.ini
+  sed -i 's/upload_max_filesize\ \=\ 2M/upload_max_filesize\ \=\ 25M/g' /etc/php5/apache2/php.ini
+  sed -i 's/post_max_size\ \=\ 8M/post_max_size\ \=\ 64M/g' /etc/php5/apache2/php.ini
+  sed -i 's/memory_limit\ \=\ 128M/memory_limit\ \=\ 184M/g' /etc/php5/apache2/php.ini
+  service apache2 restart
+}
+
+show(){
+  # Get the startup script
+  wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/subrion_hooks.sh
+  mkdir -p /CL/hooks/
+  mv subrion_hooks.sh /CL/hooks/startup.sh
+  # Execute startup script by first to get the common files
+  chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
+}
+
+if [[ -z $1 ]]; then
+  install && show
+elif [[ $1 == "show" ]]; then
+  show
+else
+  echo "unknown parameter specified"
+fi
