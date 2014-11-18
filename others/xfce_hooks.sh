e4ed3bb9c9a4a70cab50b5730a401a520fe23ce4
qmaxquique <qmax.mail@gmail.com>
2014-11-18 13:35:25 -0300

    minor changes

--- others/xfce_hooks.sh
+++ others/xfce_hooks.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-name="xfce"
+name="xguac"
 
 export PATH=$PATH:/srv/cloudlabs/scripts
-
 
 # Getting the doc and styles
 wget -q -N --timeout=2 https://raw.githubusercontent.com/terminalcloud/apps/master/docs/"$name".md
@@ -16,7 +15,8 @@ cat > /root/info.html << EOF
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="termlib.css" />
-<p id="exlink"><a id="exlink" target="_blank" href=""><b></b></a></p>
+<p id="exlink"><a id="exlink" target="_blank" href="http://$(hostname)-8080.terminal.com/guacamole/client.xhtml?id=c%2FDesktop"><b>See your Desktop here!</b></a></p>
+<p id="exlink"><a id="exlink" target="_blank" href="http://$(hostname)-8080.terminal.com/guacamole"><b>Guacamole Interface</b></a></p>
 </head>
 <body>
 EOF
@@ -31,15 +31,16 @@ cat >> /root/info.html << EOF
 EOF
 
 # Convert links to external links
-sed -i 's/a\ href/a\ target\=\"\_blank\"\ href/g' /root/info.html 
+sed -i 's/a\ href/a\ target\=\"\_blank\"\ href/g' /root/info.html
 
 # Update server URL in Docs
 sed -i "s/terminalservername/$(hostname)/g" /root/info.html
 
-# Open a new terminal with the configuration utility
-echo "/root/config.sh" | /srv/cloudlabs/scripts/run_in_term.js
 
 # Showing up
-cat | /srv/cloudlabs/scripts/run_in_term.js	 << EOF
+cat | /srv/cloudlabs/scripts/run_in_term.js      << EOF
+service vncserver start
+service guacd start
+service tomcat6 start
 /srv/cloudlabs/scripts/display.sh /root/info.html
 EOF
