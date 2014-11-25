cbd17c9d714e982d9817e1abb68b36af6637c8dd
qmaxquique <qmax.mail@gmail.com>
2014-11-25 16:59:49 -0300

    various changes

--- others/nginx-lb_cfg.sh
+++ others/nginx-lb_cfg.sh
@@ -30,7 +30,7 @@ MONGODB="264be895334c010804e5c9179f6b856e4af19f1e68ec982be49177ebcc645b02"
 
 select_sid(){
   cd /root
-  wget https://raw.githubusercontent.com/terminalcloud/apps/master/others/lb_stacks.json
+  wget https://raw.githubusercontent.com/terminalcloud/apps/master/others/lb_stack.json
   clear
   echo 'Please select the basic image for your application'
   '"1" for Ubuntu Basic Image'
@@ -42,14 +42,14 @@ select_sid(){
   '"0" OTHER - Enter your snapshot ID'
   read option
   case $option in
-    1) sed -i "s/sid/$UBUNTU/g" lb_stacks.json ;;
-    2) sed -i "s/sid/$CENTOS/g" lb_stacks.json ;;
-    3) sed -i "s/sid/$PHP/g" lb_stacks.json ;;
-    4) sed -i "s/sid/$NODEJS/g" lb_stacks.json ;;
-    5) sed -i "s/sid/$RUBY/g" lb_stacks.json ;;
-    6) sed -i "s/sid/$DJANGO/g" lb_stacks.json ;;
+    1) sed -i "s/sid/$UBUNTU/g" lb_stack.json ;;
+    2) sed -i "s/sid/$CENTOS/g" lb_stack.json ;;
+    3) sed -i "s/sid/$PHP/g" lb_stack.json ;;
+    4) sed -i "s/sid/$NODEJS/g" lb_stack.json ;;
+    5) sed -i "s/sid/$RUBY/g" lb_stack.json ;;
+    6) sed -i "s/sid/$DJANGO/g" lb_stack.json ;;
     0) custom_sid ;;
-    *) echo "Invalid option, assuming 1, Ubuntu basic Image"; sed -i "s/sid/$UBUNTU/g" lb_stacks.json ;;
+    *) echo "Invalid option, assuming 1, Ubuntu basic Image"; sed -i "s/sid/$UBUNTU/g" lb_stack.json ;;
   esac
 }
 
@@ -63,10 +63,10 @@ select_number(){
   echo '"3" for xLarge [4CPU] [6.4Gb RAM]'
   read kind
   case $kind in
-    1) sed -i "s/cpuq/100/g" lb_stacks.json && sed -i "s/ramq/1600/g" lb_stacks.json ;;
-    2) sed -i "s/cpuq/200/g" lb_stacks.json && sed -i "s/ramq/3200/g" lb_stacks.json ;;
-    3) sed -i "s/cpuq/400/g" lb_stacks.json && sed -i "s/ramq/6400/g" lb_stacks.json ;;
-    *) echo "Invalid option, assuming Small"; sed -i "s/cpuq/100/g" lb_stacks.json && sed -i "s/ramq/1600/g" lb_stacks.json ;;
+    1) sed -i "s/cpuq/100/g" lb_stack.json && sed -i "s/ramq/1600/g" lb_stack.json ;;
+    2) sed -i "s/cpuq/200/g" lb_stack.json && sed -i "s/ramq/3200/g" lb_stack.json ;;
+    3) sed -i "s/cpuq/400/g" lb_stack.json && sed -i "s/ramq/6400/g" lb_stack.json ;;
+    *) echo "Invalid option, assuming Small"; sed -i "s/cpuq/100/g" lb_stack.json && sed -i "s/ramq/1600/g" lb_stack.json ;;
   esac
 }
 
@@ -78,16 +78,16 @@ get_tokens(){
   read atoken
   echo 'Trying to generate your application servers'
 
-  sed -i "s/utoken/$utoken/g" lb_stacks.json
-  sed -i "s/atoken/$atoken/g" lb_stacks.json
-  sed -i "s/sid/$sid/g" lb_stacks.json
-  sed -i "s/IP/$IP/g" lb_stacks.json
+  sed -i "s/utoken/$utoken/g" lb_stack.json
+  sed -i "s/atoken/$atoken/g" lb_stack.json
+  sed -i "s/sid/$sid/g" lb_stack.json
+  sed -i "s/IP/$IP/g" lb_stack.json
 }
 
 create_nodes(){
     for ((i=1;i<=$num;i++));
     do
-      curl -L -X POST -H 'Content-Type: application/json' -d @lb_stacks.json api.terminal.com/v0.1/start_snapshot
+      curl -L -X POST -H 'Content-Type: application/json' -d @lb_stack.json api.terminal.com/v0.1/start_snapshot
       echo "Starting application server $i ..."
       sleep 2
     done
@@ -104,7 +104,7 @@ custom_sid(){
   echo ""
   echo "Enter the snapshot ID and press \"enter\""
   read sid
-  sed -i "s/sid/$sid/g" lb_stacks.json ;;
+  sed -i "s/sid/$sid/g" lb_stack.json ;;
 }
 
 
@@ -121,18 +121,18 @@ manual_slave(){
 }
 
 select_db(){
-  rm lb_stacks.json
-  wget https://raw.githubusercontent.com/terminalcloud/apps/master/others/lb_stacks.json
+  rm lb_stack.json
+  wget https://raw.githubusercontent.com/terminalcloud/apps/master/others/lb_stack.json
   echo "Select the DB server flavor"
   '"1" for MySQL on Ubuntu'
   '"2" for MySQL on CentOS'
   '"3" for MongoDB on Ubuntu'
   read option
   case $option in
-    1) sed -i "s/sid/$MYSQL/g" lb_stacks.json ;;
-    2) sed -i "s/sid/$MYSQLC/g" lb_stacks.json ;;
-    3) sed -i "s/sid/$MONGODB/g" lb_stacks.json ;;
-    *) echo "Invalid option, assuming 1, MySQL on Ubuntu"; sed -i "s/sid/$MYSQL/g" lb_stacks.json ;;
+    1) sed -i "s/sid/$MYSQL/g" lb_stack.json ;;
+    2) sed -i "s/sid/$MYSQLC/g" lb_stack.json ;;
+    3) sed -i "s/sid/$MONGODB/g" lb_stack.json ;;
+    *) echo "Invalid option, assuming 1, MySQL on Ubuntu"; sed -i "s/sid/$MYSQL/g" lb_stack.json ;;
   esac
 }
 
