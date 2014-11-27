fe82cd6ff72eb2fa486255d0cc7841e8c953f26e
qmaxquique <qmax.mail@gmail.com>
2014-11-27 17:15:48 -0300

    minor changes

--- others/nginx-lb_cfg.sh
+++ others/nginx-lb_cfg.sh
@@ -193,4 +193,4 @@ esac
 
 # Delete tokens
 [ -f "/root/tokens.json" ] && rm /root/tokens.json
-[ -f "/root/lb_stack.json"] && rm /root/lb_stack.json
+[ -f "/root/lb_stack.json" ] && rm /root/lb_stack.json
