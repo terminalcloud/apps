08cb736cdbfbec03bc65e4c7119775cbe18ac6c8
qmaxquique <qmax.mail@gmail.com>
2014-12-01 16:17:31 -0300

    various changes

--- others/nginx-lb_reg.sh
+++ others/nginx-lb_reg.sh
@@ -15,6 +15,3 @@ set -x
 IP=$(ip a | grep 240| awk '{print $2}' | cut -d / -f1)
 # Execute registration
 curl $HOST:5500/reg/"$SERVERKEY,$IP,$PORT,$TRIES,$TIMEOUT"
-mkdir -p /mnt/share
-sleep 2
-mount $HOST:/share /mnt/share
