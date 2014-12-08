3592e552f3ed9f3befd16d6baf3abc1b83f14baa
qmaxquique <qmax.mail@gmail.com>
2014-12-08 15:57:30 -0300

    initial commit

---
+++ booked_installer.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Script to deploy Booked at Terminal.com
+
+INSTALL_PATH="/var/www"
+
+# Includes
+wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
+source terlib.sh || (echo "cannot get the includes"; exit -1)
+
+install(){
+  # Basics
+  system_cleanup
+  basics_install
+
+  # Procedure:
+  php5_install
+  mysql_install
+  mysql_setup booked booked terminal
+  cd $INSTALL_PATH
+  wget -O booked.zip http://downloads.sourceforge.net/project/phpscheduleit/Booked/2.5/booked-2.5.12.zip
+  unzip booked.zip && rm booked.zip
+  chown -R www-data:www-data booked
+  apache_install
+  apache_default_vhost booked.conf $INSTALL_PATH/booked/
+  echo "date.timezone = America/Los_Angeles" >> /etc/php5/apache2/php.ini
+  sed -i 's/upload_max_filesize\ \=\ 2M/upload_max_filesize\ \=\ 30M/g' /etc/php5/apache2/php.ini
+  sed -i 's/post_max_size\ \=\ 8M/post_max_size\ \=\ 32M/g' /etc/php5/apache2/php.ini
+  service apache2 restart
+}
+
+show(){
+  # Get the startup script
+  wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/booked_hooks.sh
+  mkdir -p /CL/hooks/
+  mv booked_hooks.sh /CL/hooks/startup.sh
+  # Execute startup script by first to get the common files
+  chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
+}
+
+if [[ -z $1 ]]; then
+  install && show
+elif [[ $1 == "show" ]]; then
+  show
+else
+  echo "unknown parameter specified"
+fi
