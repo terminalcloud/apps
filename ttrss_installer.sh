54c15162a60aeaf368d41d3d727ef2f7e5747b13
qmaxquique <qmax.mail@gmail.com>
2014-12-10 20:24:18 -0300

    various changes

--- ttrss_installer.sh
+++ ttrss_installer.sh
@@ -14,16 +14,16 @@ install(){
   pulldocker_install
 
   # Procedure:
-  cd INSTALL_PATH
+  cd $INSTALL_PATH
 
   # First, get a PostgreSQL docker dump
   pulldocker nornagon/postgres
   # Create a startup script to keep all clear
   echo '/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf' > nornagon/postgres/start.sh
   # Do some fixes in there
-  chroot nornagon/postgres chown -R postgres:postgress /var/run/postgresql
-  chroot nornagon/postgres chown -R postgres:postgress /var/lib/postgresql/9.3/main
-  chroot nornagon/postgres chown -R postgres:postgress /etc/postgresql
+  chroot nornagon/postgres chown -R postgres /var/run/postgresql
+  chroot nornagon/postgres chown -R postgres /var/lib/postgresql/9.3/main
+  chroot nornagon/postgres chown -R postgres /etc/postgresql
 
   # Get the app docker dump
   pulldocker clue/ttrss
@@ -39,7 +39,8 @@ export DB_USER ttrss
 export DB_PASS ttrss
 php /configure-db.php && supervisord -c /etc/supervisor/conf.d/supervisord.conf
 EOF
-
+  # Fix permissions
+  chroot clue/ttrss chown -R www-data:www-data /var/www
   # Launch chroot jails
   chroot nornagon/postgres bash /start.sh &
   chroot clue/ttrss bash /start.sh &
