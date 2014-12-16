34385d8a43597c50fa72517300a518a4d560e4b9
qmaxquique <qmax.mail@gmail.com>
2014-12-16 18:20:34 -0300

    initial commit

---
+++ docs/projectsend.md
@@ -0,0 +1,52 @@
+# **ProjectSend** Terminal.com Snapshot
+
+*Share files, with your clients!*
+
+---
+
+## About ProjectSend
+
+**ProjectSend** is a self-hosted application that lets you upload files and assign them to specific clients that you create yourself! Secure, private and easy. No more depending on external services or e-mail to send those files!
+
+---
+
+## Main Features
+
+- Keep your private files secure.
+- Share with clients or groups.
+- Your clients can upload too.
+- Detailed log of actions.
+- Statistics, inactive user and clients accounts, templates, languages, and a growing community of awesome users, translators and developers that are helping ProjectSend grow day by day.
+
+---
+
+## Usage
+
+Just spin up a new Terminal based on this snapshot. Access the admin section by clicking to "ProjectSend Admin login" and login with the credentials provided (see below).
+
+### Credentials
+
+- username: `admin`
+- password: `t3rminal`
+
+---
+
+![1](IMAGE_URL)
+
+---
+
+## Documentation
+
+- [ProjectSend website](http://www.projectsend.org/)
+- [Facebook page](https://www.facebook.com/projectsend)
+
+---
+
+### Snapshot Bootstrap Script
+
+This snapshot was created from a [base snapshot](https://www.terminal.com/tiny/FzpHiTXG1K) by executing:
+`wget https://raw.githubusercontent.com/terminalcloud/apps/master/ProjectSend_installer.sh && bash ProjectSend_installer.sh`
+
+---
+
+#### Thanks for using ProjectSend at Terminal.com!
---
+++ projectsend_installer.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Script to deploy ProjectSend at Terminal.com
+
+INSTALL_PATH="/var/www"
+
+# Includes
+wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
+source terlib.sh || (echo "cannot get the includes"; exit -1)
+
+install(){
+  # Basics
+  system_cleanup
+  basics_install
+
+  # Procedure:
+  php5_install
+  mysql_install
+  mysql_setup projectsend projectsend terminal
+  cd $INSTALL_PATH
+  mkdir projectsend && cd projectsend
+  wget https://raw.githubusercontent.com/terminalcloud/apps/master/others/projectsend.zip
+  unzip projectsend.zip && rm projectsend.zip
+  cd $INSTALL_PATH
+  chown -R www-data:www-data projectsend
+  apache_install
+  apache_default_vhost projectsend.conf $INSTALL_PATH/projectsend/
+  echo "date.timezone = America/Los_Angeles" >> /etc/php5/apache2/php.ini
+  sed -i 's/upload_max_filesize\ \=\ 2M/upload_max_filesize\ \=\ 30M/g' /etc/php5/apache2/php.ini
+  sed -i 's/post_max_size\ \=\ 8M/post_max_size\ \=\ 32M/g' /etc/php5/apache2/php.ini
+  service apache2 restart
+}
+
+show(){
+  # Get the startup script
+  wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/projectsend_hooks.sh
+  mkdir -p /CL/hooks/
+  mv projectsend_hooks.sh /CL/hooks/startup.sh
+  # Execute startup script by first to get the common files
+  chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
+}
+
+if [[ -z $1 ]]; then
+  install && show
+elif [[ $1 == "show" ]]; then
+  show
+else
+  echo "unknown parameter specified"
+fi
