f959ab0f860aa7f2e89dda1fcdb078ae80ed042d
Administrator <admin@example.com>
2015-02-20 16:22:05 -0300

    xguac.d updates

--- others/xguac_hooks.sh
+++ others/xguac_hooks.sh
@@ -8,6 +8,9 @@ export PATH=$PATH:/srv/cloudlabs/scripts
 wget -q -N --timeout=2 https://raw.githubusercontent.com/terminalcloud/apps/master/docs/"$name".md
 wget -q -N --timeout=2 https://raw.githubusercontent.com/terminalcloud/apps/master/docs/termlib.css && mv termlib.css /root/
 
+service vncserver start
+service guacd start
+service tomcat7 start
 
 # Making the file...
 cat > /root/info.html << EOF
@@ -15,8 +18,8 @@ cat > /root/info.html << EOF
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="termlib.css" />
-<p id="exlink"><a id="exlink" target="_blank" href="http://$(hostname)-8080.terminal.com/guacamole/client.xhtml?id=c%2FDesktop"><b>See your Desktop here!</b></a></p>
-<p id="exlink"><a id="exlink" target="_blank" href="http://$(hostname)-8080.terminal.com/guacamole"><b>Guacamole Interface</b></a></p>
+<p id="exlink"><a id="exlink" target="_blank" href="http://$(hostname)-8080.terminal.com/guacamole/#/client/c/Desktop"><b>See your Desktop here!</b></a></p>
+<p id="exlink"><a id="exlink" target="_blank" href="http://$(hostname)-8080.terminal.com/guacamole/#"><b>Guacamole Interface</b></a></p>
 </head>
 <body>
 EOF
@@ -36,11 +39,7 @@ sed -i 's/a\ href/a\ target\=\"\_blank\"\ href/g' /root/info.html
 # Update server URL in Docs
 sed -i "s/terminalservername/$(hostname)/g" /root/info.html
 
-
 # Showing up
 cat | /srv/cloudlabs/scripts/run_in_term.js	 << EOF
-service vncserver start
-service guacd start
-service tomcat6 start
 /srv/cloudlabs/scripts/display.sh /root/info.html
 EOF
