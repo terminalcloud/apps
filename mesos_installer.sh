e6e4fc49af6d32530abf0bf6f1a24826a2446cef
qmaxquique <qmax.mail@gmail.com>
2015-03-31 16:03:36 -0300

    various changes

--- mesos_installer.sh
+++ mesos_installer.sh
@@ -47,7 +47,33 @@ compile(){
     make install
 }
 
+spark_installer(){
+    apt-get -y install build-essential
+    sudo aptitude -y install openjdk-7-jdk
+    wget http://d3kbcqa49mib13.cloudfront.net/spark-0.8.0-incubating.tgz
+    tar xzf spark-0.8.0-incubating.tgz
+    cd spark-0.8.0-incubating/
+    SPARK_HADOOP_VERSION=2.0.0-mr1-cdh4.4.0 sbt/sbt clean assembly
+    ./make-distribution.sh --hadoop 2.0.0-mr1-cdh4.4.0
+    mv dist spark-0.8.0-2.0.0-mr1-cdh4.4.0
+    tar czf spark-0.8.0-2.0.0-mr1-cdh4.4.0.tgz spark-0.8.0-2.0.0-mr1-cdh4.4.0
+    hadoop fs -mkdir /tmp
+    hadoop fs -put spark-0.8.0-2.0.0-mr1-cdh4.4.0.tgz /tmp
+    cd conf/
+    cp spark-env.sh.template spark-env.sh
+    IP=$(/sbin/ifconfig $1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' | grep 240)
+    #####################################
+    cat > spark-env.sh << EOF
+export MESOS_NATIVE_LIBRARY=/usr/local/lib/libmesos.so
+export SPARK_EXECUTOR_URI=hdfs://54.204.196.36/tmp/spark-0.8.0-2.0.0-mr1-cdh4.4.0.tgz
+export MASTER=zk://$IP:2181/mesos
+EOF
+    cd ..
+    ####### SCALA? #######
+    ./spark-shell
 
+
+}
 
 show(){
 	# Get the startup script
