53f1c1212e7b9236fa07d8fe2c5eeace117ff648
qmaxquique <qmax.mail@gmail.com>
2015-04-17 16:14:35 -0300

    License notes added to all shell script files.

---
+++ license_adder.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# Small helper script to add License header to shell scripts
+# Cloudlabs, INC. Copyright (C) 2015
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Cloudlabs, INC. - 653 Harrison St, San Francisco, CA 94107.
+# http://www.terminal.com - help@terminal.com
+
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Cloudlabs, INC. - 653 Harrison St, San Francisco, CA 94107.
+# http://www.terminal.com - help@terminal.com
+
+
+HEADER=$1
+TARGET=$2
+
+TSIZE="$(wc -l $TARGET | awk '{print $1}')"
+QSIZE="$(( $TSIZE - 2 ))"
+
+tail "-$QSIZE" "$TARGET" > "$TARGET.tail"
+head -2 "$TARGET" > "$TARGET.head"
+cat "$TARGET.head" "$HEADER" "$TARGET.tail" > "$TARGET"
+rm "$TARGET.head" "$TARGET.tail"
--- mesos_installer.sh
+++ mesos_installer.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 # Script to deploy Mesos (Stand-Alone Version) at Terminal.com
+# Cloudlabs, INC. Copyright (C) 2015
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Cloudlabs, INC. - 653 Harrison St, San Francisco, CA 94107.
+# http://www.terminal.com - help@terminal.com
 
 INSTALL_PATH="/root"
 
@@ -47,23 +65,56 @@ compile(){
     make install
 }
 
+hadoop_installer(){
+    apt-get install -y openjdk-7-jdk
+    wget http://mirrors.nxnethosting.com/apache/hadoop/common/current/hadoop-2.6.0.tar.gz
+    tar xvzf hadoop-2.6.0.tar.gz
+    mv hadoop-2.6.0 /usr/local/hadoop
+
+cat >> .bash_rc << EOF
+#HADOOP VARIABLES START
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export HADOOP_INSTALL=/usr/local/hadoop
+export PATH=$PATH:$HADOOP_INSTALL/bin
+export PATH=$PATH:$HADOOP_INSTALL/sbin
+export HADOOP_MAPRED_HOME=$HADOOP_INSTALL
+export HADOOP_COMMON_HOME=$HADOOP_INSTALL
+export HADOOP_HDFS_HOME=$HADOOP_INSTALL
+export YARN_HOME=$HADOOP_INSTALL
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_INSTALL/lib/native
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_INSTALL/lib"
+#HADOOP VARIABLES END
+EOF
+
+#seguir aca
+
+}
+
+
 spark_installer(){
-    apt-get -y install build-essential
-    sudo aptitude -y install openjdk-7-jdk
-    wget http://d3kbcqa49mib13.cloudfront.net/spark-0.8.0-incubating.tgz
-    tar xzf spark-0.8.0-incubating.tgz
-    cd spark-0.8.0-incubating/
-    SPARK_HADOOP_VERSION=2.0.0-mr1-cdh4.4.0 sbt/sbt clean assembly
-    ./make-distribution.sh --hadoop 2.0.0-mr1-cdh4.4.0
-    mv dist spark-0.8.0-2.0.0-mr1-cdh4.4.0
-    tar czf spark-0.8.0-2.0.0-mr1-cdh4.4.0.tgz spark-0.8.0-2.0.0-mr1-cdh4.4.0
-    hadoop fs -mkdir /tmp
-    hadoop fs -put spark-0.8.0-2.0.0-mr1-cdh4.4.0.tgz /tmp
-    cd conf/
-    cp spark-env.sh.template spark-env.sh
-    IP=$(/sbin/ifconfig $1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' | grep 240)
-    #####################################
-    cat > spark-env.sh << EOF
+echo "deb http://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+apt-get update
+apt-get install -y sbt build-essential openjdk-7-jdk scala
+
+# Hadoop
+
+
+
+
+wget http://d3kbcqa49mib13.cloudfront.net/spark-0.8.0-incubating.tgz
+tar xzf spark-0.8.0-incubating.tgz
+cd spark-0.8.0-incubating/
+SPARK_HADOOP_VERSION=2.0.0-mr1-cdh4.4.0 sbt/sbt clean assembly
+./make-distribution.sh --hadoop 2.0.0-mr1-cdh4.4.0
+mv dist spark-0.8.0-2.0.0-mr1-cdh4.4.0
+tar czf spark-0.8.0-2.0.0-mr1-cdh4.4.0.tgz spark-0.8.0-2.0.0-mr1-cdh4.4.0
+hadoop fs -mkdir /tmp
+hadoop fs -put spark-0.8.0-2.0.0-mr1-cdh4.4.0.tgz /tmp
+cd conf/
+cp spark-env.sh.template spark-env.sh
+IP=$(/sbin/ifconfig $1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' | grep 240)
+#####################################
+cat > spark-env.sh << EOF
 export MESOS_NATIVE_LIBRARY=/usr/local/lib/libmesos.so
 export SPARK_EXECUTOR_URI=hdfs://54.204.196.36/tmp/spark-0.8.0-2.0.0-mr1-cdh4.4.0.tgz
 export MASTER=zk://$IP:2181/mesos
