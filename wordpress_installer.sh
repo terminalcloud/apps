8c9b05f90daf64870e7e027485ca1028120d2085
qmaxquique <qmax.mail@gmail.com>
2015-06-17 13:57:02 -0300

    wordpress fix

--- wordpress_installer.sh
+++ wordpress_installer.sh
@@ -37,8 +37,8 @@ install(){
 	mysql_install
 	mysql_setup wordpress wordpress terminal
 	cd $INSTALL_PATH
-  wget http://wordpress.org/latest.tar.gz
-  tar xzvf latest.tar.gz && rm latest.tar.gz
+    wget http://wordpress.org/latest.tar.gz
+    tar xzvf latest.tar.gz && rm latest.tar.gz
 	chown -R www-data:www-data wordpress
 	apache_install
 	apache_default_vhost wordpress.conf $INSTALL_PATH/wordpress
@@ -46,26 +46,26 @@ install(){
 	sed -i 's/post_max_size\ \=\ 8M/post_max_size\ \=\ 32M/g' /etc/php5/apache2/php.ini
 	sed -i 's/memory_limit\ \=\ 128M/memory_limit\ \=\ 256M/g' /etc/php5/apache2/php.ini
 
-  # 3 - Configuring...
-  cd $INSTALL_PATH/wordpress
-  cp wp-config-sample.php wp-config.php
-  sed -i "s/database_name_here/wordpress/g" wp-config.php
-  sed -i "s/username_here/wordpress/g" wp-config.php
-  sed -i "s/password_here/terminal/g" wp-config.php
+     # 3 - Configuring...
+    cd $INSTALL_PATH/wordpress
+    cp wp-config-sample.php wp-config.php
+    sed -i "s/database_name_here/wordpress/g" wp-config.php
+    sed -i "s/username_here/wordpress/g" wp-config.php
+    sed -i "s/password_here/terminal/g" wp-config.php
 
-  mkdir $INSTALL_PATH/wordpress/wp-content/uploads
-  chown -R www-data:www-data $INSTALL_PATH/wordpress
+    mkdir $INSTALL_PATH/wordpress/wp-content/uploads
+    chown -R www-data:www-data $INSTALL_PATH/wordpress
 
-	service apache2 restart
+    service apache2 restart
 
   # 4 - Install wp-cli
-  cd /root
-  wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-  chmod +x wp-cli.phar
-  mv wp-cli.phar /usr/local/bin/wp
-  wget https://github.com/wp-cli/wp-cli/raw/master/utils/wp-completion.bash
-  mv wp-completion.bash .wp-completion.bash
-  echo "source .wp-completion.bash" >> .bashrc
+    cd /root
+    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+    wget https://github.com/wp-cli/wp-cli/raw/master/utils/wp-completion.bash
+    mv wp-completion.bash .wp-completion.bash
+    echo "source .wp-completion.bash" >> .bashrc
 }
 
 show(){
