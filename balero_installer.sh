189119fd2d558a9c81d6cc3a455dab02d49c5abc
qmaxquique <qmax.mail@gmail.com>
2015-07-06 11:36:23 -0300

    seopanel added

---
+++ balero_installer.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Script to deploy Balero CMS at Terminal.com
+# Cloudlabs, INC. Copyright (C) 2015
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Cloudlabs, INC. - 653 Harrison St, San Francisco, CA 94107.
+# http://www.terminal.com - help@terminal.com
+
+
+INSTALL_PATH="/var/www"
+
+# Includes
+wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
+source terlib.sh || (echo "cannot get the includes"; exit -1)
+
+install(){
+  # Basics
+  system_cleanup
+  basics_install
+
+  # Procedure:
+  php5_install
+  mysql_install
+  mysql_setup balero balero terminal
+  cd $INSTALL_PATH
+  wget -O balero.zip https://github.com/BaleroCMS/balerocms-src/archive/master.zip
+  unzip balero.zip && rm balero.zip
+  mv balerocms-src-master balero
+  chown -R www-data:www-data balero
+  mv /var/www/balero/site/etc/balero.config.xml.blank /var/www/balero/site/etc/balero.config.xml
+  apache_install
+  apache_default_vhost balero.conf $INSTALL_PATH/balero/
+  echo "date.timezone = America/Los_Angeles" >> /etc/php5/apache2/php.ini
+  sed -i 's/upload_max_filesize\ \=\ 2M/upload_max_filesize\ \=\ 30M/g' /etc/php5/apache2/php.ini
+  sed -i 's/post_max_size\ \=\ 8M/post_max_size\ \=\ 32M/g' /etc/php5/apache2/php.ini
+  sed -i 's/max_execution_time\ \=\ 30/max_execution_time\ \=\ 60/g' /etc/php5/apache2/php.ini
+  service apache2 restart
+}
+
+show(){
+  # Get the startup script
+  wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/balero_hooks.sh
+  mkdir -p /CL/hooks/
+  mv balero_hooks.sh /CL/hooks/startup.sh
+  # Execute startup script by first to get the common files
+  chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
+}
+
+if [[ -z $1 ]]; then
+  install && show
+elif [[ $1 == "show" ]]; then
+  show
+else
+  echo "unknown parameter specified"
+fi
