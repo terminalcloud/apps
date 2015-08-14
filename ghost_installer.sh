2e449d39a40cb10f05f1ceaf972c02df0a909f13
qmaxquique <qmax.mail@gmail.com>
2015-08-14 16:53:27 -0300

    Adding Ghost installer

--- ghost_installer.sh
+++ ghost_installer.sh
@@ -63,7 +63,7 @@ show(){
 	# Get the startup script
 	wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/ghost_hooks.sh
 	mkdir -p /CL/hooks/
-	mv nodebb_hooks.sh /CL/hooks/startup.sh
+	mv ghost_hooks.sh /CL/hooks/startup.sh
 	# Execute startup script by first to get the common files
 	chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
 }
