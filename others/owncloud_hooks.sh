097750170a619b2c23f37bee6ab8b4c8dadcbdb8
qmaxquique <qmax.mail@gmail.com>
2015-08-17 10:56:09 -0300

    bugfixes

--- others/owncloud_hooks.sh
+++ others/owncloud_hooks.sh
@@ -19,14 +19,12 @@
 # Cloudlabs, INC. - 653 Harrison St, San Francisco, CA 94107.
 # http://www.terminal.com - help@terminal.com
 
-
+export name='owncloud'
 export PATH=$PATH:/srv/cloudlabs/scripts
 
 # Getting the doc and styles
 wget -q -N --timeout=2 https://raw.githubusercontent.com/terminalcloud/apps/master/docs/"$name".md
 wget -q -N --timeout=2 https://raw.githubusercontent.com/terminalcloud/apps/master/docs/termlib.css && mv termlib.css /root/
-
-sed -i
 
 # Making the file...
 cat > /root/info.html << EOF
