613791a0a5e08c7fbddddd4969ce74bc290713f8
qmaxquique <qmax.mail@gmail.com>
2015-10-02 18:44:29 -0300

    bugfixes

--- huginn_installer.sh
+++ huginn_installer.sh
@@ -77,13 +77,18 @@ install(){
 }
 
 show(){
-	true
+  # Get the startup script
+  wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/huginn_hooks.sh
+  mkdir -p /CL/hooks/
+  mv huginn_hooks.sh /CL/hooks/startup.sh
+  # Execute startup script by first to get the common files
+  chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
 }
 
 if [[ -z $1 ]]; then
-	install && show
-elif [[ $1 == "show" ]]; then 
-	show
+  install && show
+elif [[ $1 == "show" ]]; then
+  show
 else
-	echo "unknown parameter specified"
+  echo "unknown parameter specified"
 fi
--- others/huginn_setup.sh
+++ others/huginn_setup.sh
@@ -19,21 +19,71 @@
 # Cloudlabs, INC. - 653 Harrison St, San Francisco, CA 94107.
 # http://www.terminal.com - help@terminal.com
 
-
-cd /home/huginn/huginn
-    # From here, customizations
-token=$(sudo -u huginn -H rake secret)
-sudo -u huginn -H sed -i 's/APP_SECRET_TOKEN\=.*/APP_SECRET_TOKEN\='$token'/g' .env
-sudo -u huginn -H sed -i 's/^DOMAIN=.*/DOMAIN='$(hostname)'-3000.terminal.com/g' .env
+# Set home dir
+WORKDIR='/home/huginn/huginn'
 
 
-sudo -u huginn -H bundle exec rake db:create RAILS_ENV=production
-sudo -u huginn -H bundle exec rake db:migrate RAILS_ENV=production
-sudo -u huginn -H bundle exec rake db:seed RAILS_ENV=production
+setup_domain(){
+    read -p "Please write down your domain carefuly and press enter:" domain
+    sudo -u huginn -H sed -i 's/^DOMAIN=.*/DOMAIN='$domain'-3000.terminal.com/g' .env
+}
 
-# Check the username and stuff here
-sudo -u huginn -H bundle exec rake db:seed RAILS_ENV=production SEED_USERNAME=admin SEED_PASSWORD=terminal
-sudo -u huginn -H bundle exec rake assets:precompile RAILS_ENV=production
 
-#rake production:export
+custom_smtp(){
+    echo "Please provide the information needed."
+    read -p "SMTP Domain (e.g.: gmail.com)?:" SMTP_DOMAIN
+    read -p "SMTP User Name (e.g.: you@gmail.com)?:" SMTP_USER_NAME
+    read -p "SMTP Server (e.g.: smtp.gmail.com):" SMTP_SERVER
+    read -s -p "Password (Will not be shown):" SMTP_PASS
+    echo 'OK'
+    sudo -u huginn -H sed -i 's/SMTP_DOMAIN\=.*/SMTP_DOMAIN\='$SMTP_DOMAIN'/g' .env
+    sudo -u huginn -H sed -i 's/SMTP_USER_NAME\=.*/SMTP_USER_NAME\='$SMTP_USER_NAME'/g' .env
+    sudo -u huginn -H sed -i 's/SMTP_SERVER\=.*/SMTP_SERVER\='$SMTP_SERVER'/g' .env
+    sudo -u huginn -H sed -i 's/SMTP_PASSWORD\=.*/SMTP_PASSWORD\='$SMTP_PASS'/g' .env
+}
+
+
+
+main_process(){
+    # Setup uniq token
+    token=$(sudo -u huginn -H rake secret)
+    sudo -u huginn -H sed -i 's/APP_SECRET_TOKEN\=.*/APP_SECRET_TOKEN\='$token'/g' .env
+
+    # Ask for custom domains or setup the default one
+    while true; do
+        read -p "Do you wish to use your custom domain on this installation?" yn_domain
+        case $yn_domain in
+            [Yy]* ) setup_domain; break;;
+            [Nn]* ) sudo -u huginn -H sed -i 's/^DOMAIN=.*/DOMAIN='$(hostname)'-3000.terminal.com/g' .env ; break;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+
+    # Ask for SMTP stuff
+    while true; do
+        read -p "Do you want to setup your SMTP credentials?" yn_smtp
+        case $yn_smtp in
+            [Yy]* ) custom_smtp; break;;
+            [Nn]* ) true; break;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+    echo 'Creating Database - Please stand by...'
+    sudo -u huginn -H bundle exec rake db:create RAILS_ENV=production
+    sudo -u huginn -H bundle exec rake db:migrate RAILS_ENV=production
+    sudo -u huginn -H bundle exec rake db:seed RAILS_ENV=production
+
+    # Check the username and stuff here
+    read -p "Please provide an Admin user name for Huginn:" USER
+    read -s -p "Password (Will not be shown): " PASS
+    echo "OK"
+    sudo -u huginn -H bundle exec rake db:seed RAILS_ENV=production SEED_USERNAME=$USER SEED_PASSWORD=$PASS
+    sudo -u huginn -H bundle exec rake assets:precompile RAILS_ENV=production
+    echo "please wait..."
+    sleep 3
+}
+
+cd $WORKDIR
+main_process
+sudo -u root -H rake production:export
 cp deployment/logrotate/huginn /etc/logrotate.d/huginn
