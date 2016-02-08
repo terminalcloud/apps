069b779be6c27a26a6a3574906af6e4353480c72
qmaxquique <qmax.mail@gmail.com>
2016-02-08 17:33:25 -0300

    installer added to the repo

---
+++ docs/webmin.md
@@ -0,0 +1,41 @@
+# **Werbmin** Terminal.com Snapshot
+
+*Minimal stack for running Terminal.com IDE and Webmin web administration tool.*
+
+---
+
+## About Webmin
+
+Webmin is a web-based interface for system administration for Unix. 
+Using any modern web browser, you can setup user accounts, Apache, DNS, file sharing and much more
+
+---
+
+## Usage
+
+Webmin is running on port 10000 by default. 
+To access webmin just go to https://terminalservername.terminal.com and login with root credentials.
+
+### Credentails:
+
+- User: root
+- Password: t3rminal
+
+
+
+#### Important Note:
+
+The first time you login webmin (immediately after login) it will lead you to a blank screen at https://terminal46224-1000.terminal.com:10000. 
+This is due a webmin harcoded redirection. To bypass that issue, just press "back" in your browser or refresh you browser before it happens.
+
+---
+
+## Documentation
+
+- [Webmin Main Site](http://www.webmin.com/)
+- [Official Documentation](http://www.webmin.com/docs.html)
+- [Frequently Asked Questions](http://www.webmin.com/faq.html)
+
+---
+
+#### Thanks for using this Terminal Snapshot!
---
+++ webstarterkit_installer.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Script to deploy PHPBB at Terminal.com
+# Cloudlabs, INC. Copyright (C) 2015
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Cloudlabs, INC. - 653 Harrison St, San Francisco, CA 94107.
+# http://www.terminal.com - help@terminal.com
+
+INSTALL_PATH="/var/www"
+
+# Includes
+wget https://raw.githubusercontent.com/terminalcloud/apps/master/terlib.sh
+source terlib.sh || (echo "cannot get the includes"; exit -1)
+
+install(){
+	# Basics
+	pkg_update
+	system_cleanup
+	basics_install
+
+	# Procedure:
+    cd $INSTALL_PATH
+    git clone https://github.com/google/web-starter-kit.git
+    cd web-starter-kit
+    npm install
+    npm install -g gulp
+    npm install --save-dev babel-core
+    npm install --save-dev babel-register
+}
+
+show(){
+	# Get the startup script
+	wget -q -N https://raw.githubusercontent.com/terminalcloud/apps/master/others/webstarterkit_hooks.sh
+	mkdir -p /CL/hooks/
+	mv webstarterkit_hooks.sh /CL/hooks/startup.sh
+	# Execute startup script by first to get the common files
+	chmod 777 /CL/hooks/startup.sh && /CL/hooks/startup.sh
+}
+
+if [[ -z $1 ]]; then
+	install && show
+elif [[ $1 == "show" ]]; then 
+	show
+else
+	echo "unknown parameter specified"
+fi
